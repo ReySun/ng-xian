5dde60bd1afbe68a3dc16a9369e67817a5fefea6
ReySun <1423993690@qq.com>
2017-07-04 23:48:41 +0800

    新增pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { OutputComponent } from './output/output.component';
 import { MailServiceService } from './service/mail-service.service';
 import { NgclassComponent } from './ngclass/ngclass.component';
 import { HttpComponent } from './http/http.component';
+
 import { FirstDirective } from './directive/first.directive';
 import { SecondDirective } from './directive/second.directive';
 import { ThirdDirective } from './directive/third.directive';
@@ -17,13 +18,17 @@ import { FiveDirective } from './directive/five.directive';
 import { NgtplComponent } from './directive/ngtpl/ngtpl.component';
 import { NgtplcontextComponent } from './directive/ngtpl/ngtplcontext.component';
 import { NgtploutletComponent } from './directive/ngtpl/ngtploutlet.component';
+
 import { FirstFormComponent } from './form/first.component';
 import { SecondFormComponent } from './form/second.component';
 import { ThirdFormComponent } from './form/third.component';
 import { FourFormComponent } from './form/four.component';
+
 import { FirstPipeComponent } from './pipe/first-pipe.component';
 import { WelcomePipe } from './pipe/welcome.pipe';
 import { RepeatPipe } from './pipe/repeat.pipe';
+import { TrimSpacePipe } from './pipe/trim-space.pipe';
+
 import { AnimateComponent } from './animate/animate.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
@@ -47,6 +52,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 		FirstPipeComponent,
 		WelcomePipe,
 		RepeatPipe,
+		TrimSpacePipe,
 		AnimateComponent
 	],
 	imports: [
--- src/app/pipe/first-pipe.component.ts
+++ src/app/pipe/first-pipe.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
 	styles: [``]
 })
 export class FirstPipeComponent implements OnInit {
+	pi: number = 3.141592652412432356;
+	a: number = 100;
+	b: number = 100;
+	myNum: number = 0.1415927;
+	object: Object = {
+		foo: 'bar',
+		baz: 'qux',
+		nested: {
+			xyz: 3,
+			numbers: [1, 2, 3, 4, 5]
+		}
+	};
 	today = new Date();
 	constructor() { }
 
